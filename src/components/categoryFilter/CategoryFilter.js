17790cb240f03c7b488b859c726da875ccb7658f
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2021-05-04 10:46:19 +0600

    Fixed all the tests

--- src/components/categoryFilter/CategoryFilter.js
+++ src/components/categoryFilter/CategoryFilter.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 
 const CategoryFilter = ({ handleChange, categories }) => (
   <select name="categories" onChange={handleChange} defaultValue="default">
-    <option value="default" disabled>Select a Category</option>
+    <option value="default" disabled>Select a category</option>
     {categories.map(item => <option key={`${item.strCategory}_option`} value={item.strCategory}>{item.strCategory}</option>)}
   </select>
 );
